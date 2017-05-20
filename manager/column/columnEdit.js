a8a131d33b4f08c35e4b8bf647ff6cf0bf8ccc52
huanStephen <huanStephen@163.com>
2017-05-20 22:00:13 +0800

    栏目开发完毕

--- manager/column/columnEdit.js
+++ manager/column/columnEdit.js
@@ -7,7 +7,7 @@
     var ColumnEntity = new sepa.Class([Column.model, sepa.Model]);
 
     var ColumnEditController = new sepa.Class([sepa.Controller, sepa.CRemote, sepa.CElement, sepa.CDomRenderRole,
-        sepa.CVaildate, sepa.CCombVaildate, this.mimosa.EditTemplate]);
+        sepa.CVaildate, sepa.CCombVaildate, this.mimosa.EditTemplate, sepa.CStorage]);
 
     ColumnEditController.include({
 
@@ -15,16 +15,28 @@
 
         elements : {
             //标题
+            'h3.page-title > span:first' : 't',
+            //导航
+            'ul.breadcrumb' : 'navContainer',
+            //标题
             'input[data-field="title"]' : 'title',
             //描述
             'textarea[data-field="description"]' : 'description'
+        },
+
+        events : {
+            'click a[data-parentId]' : 'navClick'
+        },
+
+        blocks : {
+            'navBlk' : 'navEl'
         },
 
         config : {
             getInfo : {
                 path : 'column/getColumn',
                 params : {
-                    columnId : ''
+                    columnId : 0
                 },
                 callback : 'getInfoResult'
             }
@@ -41,19 +53,92 @@
             updateConfigPath : 'column/updateColumn'
         },
 
+        level : [],
+
         load : function() {
             this.info = new ColumnEntity();
             this.info.loadSession('rowInfo');
             this.info.removeSession('rowInfo');
 
+            this.renderNav();
+
             if(this.info.id) {
                 this.config.getInfo.params.columnId = this.info.id;
                 this.loadInfo();
             }
+
+        },
+
+        renderNav : function() {
+            var lv = this.component('loadSession', ['level']);
+            this.component('removeSession', ['level']);
+            lv = lv || this.level;
+
+            this.navContainer.empty();
+
+            var spl, $nav = null, $title = null;
+            for(var i=0; i < lv.length; i++) {
+                spl = lv[i].split(',');
+                $nav = this.navEl.clone();
+                if(i == 0) {
+                    $nav.children('i').addClass('fa-home');
+                } else {
+                    $nav.children('i').addClass('fa-angle-right');
+                }
+
+                $title = this.component('element', ['a']).clone();
+                $title.attr('href', 'javascript:void(0);');
+
+                $title.attr('data-parentid', spl[0]).text(spl[1]);
+
+                $nav.append($title);
+
+                this.navContainer.append($nav);
+            }
+
+            $nav = this.navEl.clone();
+            $nav.children('i').addClass('fa-angle-right');
+            $title = this.component('element', ['span']).clone();
+            if(this.info.id) {
+                this.t.text('编辑');
+                $title.text('编辑');
+            } else {
+                this.t.text('添加');
+                $title.text('添加');
+            }
+            $nav.append($title);
+
+            this.navContainer.append($nav);
+
+            this.level = lv;
+        },
+
+        navClick : function(event) {
+            var parentId = this.$(event.target).data('parentid');
+
+            for(var i=this.level.length - 1; i >= 0; i--) {
+                var spl = this.level[i].split(',');
+                if(spl[0] == parentId) {
+                    this.level.splice(i + 1, this.level.length - i - 1);
+
+                    this.component('saveSession', ['level', this.level]);
+                    indexCtrl.loadPage('column/index.html');
+                    break;
+                }
+            }
+        },
+
+        submitBefore : function() {
+            this.component('saveSession', ['level', this.level]);
         },
 
         backClick : function() {
-            history.back();
+            this.component('saveSession', ['level', this.level]);
+            indexCtrl.loadPage('column/index.html');
+        },
+
+        navBlk : function() {
+            return '<li><i class="fa"></i></li>';
         }
 
     });
--- manager/column/index.js
+++ manager/column/index.js
@@ -6,11 +6,24 @@
 
     var ColumnEntity = new sepa.Class([Column.model, sepa.Model]);
 
-    var ColumnListController = new sepa.Class([sepa.Controller, sepa.CRemote, sepa.CElement, sepa.CDomRenderRole, this.mimosa.ListTemplate]);
+    var ColumnListController = new sepa.Class([sepa.Controller, sepa.CRemote, sepa.CElement, sepa.CDomRenderRole,
+        this.mimosa.ListTemplate, sepa.CStorage]);
 
     ColumnListController.include({
 
         Model : ColumnEntity,
+
+        elements : {
+            'ul.breadcrumb' : 'navContainer'
+        },
+
+        events : {
+            'click a[data-parentId]' : 'navClick'
+        },
+
+        blocks : {
+            'navBlk' : 'navEl'
+        },
 
         config : {
             getList : {
@@ -31,11 +44,17 @@
             add : {
                 page : 'column/columnEdit.html',
                 requireRowInfo : true,
-                after : 'addAfter'
+                before : 'addBefore'
+            },
+            entry : {
+                page : 'column/index.html',
+                requireRowInfo : false,
+                before : 'entryBefore'
             },
             edit : {
                 page : 'column/columnEdit.html',
-                requireRowInfo : true
+                requireRowInfo : true,
+                before : 'editBefore'
             },
             delete : {
                 before : 'deleteBefore',
@@ -43,16 +62,86 @@
             }
         },
 
+        level : ['0,主栏目'],
+
         load : function() {
+            this.renderNav();
+
+            this.config.getList.params.parentId = this.level[this.level.length - 1].split(',')[0];
             this.loadList();
         },
 
-        addAfter : function() {
-            console.log('addAfter');
+        renderNav : function() {
+            var lv = this.component('loadSession', ['level']);
+            this.component('removeSession', ['level']);
+            lv = lv || this.level;
+
+            this.navContainer.empty();
+
+            var spl, $nav = null, $title = null;
+            for(var i=0; i < lv.length; i++) {
+                spl = lv[i].split(',');
+                $nav = this.navEl.clone();
+                if(i == 0) {
+                    $nav.children('i').addClass('fa-home');
+                } else {
+                    $nav.children('i').addClass('fa-angle-right');
+                }
+
+                if(i == lv.length - 1) {
+                    $title = this.component('element', ['span']).clone();
+                } else {
+                    $title = this.component('element', ['a']).clone();
+                    $title.attr('href', 'javascript:void(0);');
+                }
+
+                $title.attr('data-parentid', spl[0]).text(spl[1]);
+
+                $nav.append($title);
+
+                this.navContainer.append($nav);
+            }
+            this.level = lv;
+        },
+
+        navClick : function(event) {
+            var parentId = this.$(event.target).data('parentid');
+
+            for(var i=this.level.length - 1; i >= 0; i--) {
+                var spl = this.level[i].split(',');
+                if(spl[0] == parentId) {
+                    this.level.splice(i + 1, this.level.length - i - 1);
+
+                    this.component('saveSession', ['level', this.level]);
+                    indexCtrl.loadPage('column/index.html');
+                    break;
+                }
+            }
+        },
+
+        addBefore : function() {
+            this.component('saveSession', ['level', this.level]);
+        },
+
+        entryBefore : function(event) {
+            var id = this.getRowId(event);
+            var title = this.$(event.target).parents('tr').find('*[data-field="title"]').text();
+
+            this.level.push(id + ',' + title);
+
+            this.component('saveSession', ['level', this.level]);
+        },
+
+        editBefore : function() {
+            this.component('saveSession', ['level', this.level]);
         },
 
         deleteBefore : function(event) {
             this.config.deleteRow.params.columnId = this.getRowId(event);
+        },
+
+        navBlk : function() {
+            return '<li><i class="fa"></i></li>';
         }
 
     });
