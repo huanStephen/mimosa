d24856d9f96fa0dccb265670307f8342b36aa3a4
huanStephen <huanStephen@163.com>
2017-05-25 14:23:50 +0800

    创建资源展示页

--- manager/resource/index.js
+++ manager/resource/index.js
@@ -13,18 +13,6 @@
 
         Model : ResourceAlbumEntity,
 
-        /*elements : {
-            'ul.breadcrumb' : 'navContainer'
-        },
-
-        events : {
-            'click a[data-parentId]' : 'navClick'
-        },
-
-        blocks : {
-            'navBlk' : 'navEl'
-        },*/
-
         elements : {
             'h3.page-title' : 'title'
         },
@@ -46,17 +34,17 @@
 
         operateConfig : {
             add : {
-                page : 'column/columnEdit.html',
+                page : 'resource/resourceAlbumEdit.html',
                 requireRowInfo : false,
                 before : 'addBefore'
             },
             entry : {
-                page : 'column/index.html',
-                requireRowInfo : false,
+                page : '',
+                requireRowInfo : true,
                 before : 'entryBefore'
             },
             edit : {
-                page : 'column/columnEdit.html',
+                page : 'resource/resourceAlbumEdit.html',
                 requireRowInfo : true,
                 before : 'editBefore'
             },
@@ -66,10 +54,7 @@
             }
         },
 
-        //level : ['0,����Ŀ'],
-
         load : function() {
-            //this.renderNav();
             this.resourceType = location.hash.slice(1);
 
             if(this.resourceType == 'image') {
@@ -89,78 +74,17 @@
             this.loadList();
         },
 
-        /*renderNav : function() {
-            var lv = this.component('loadSession', ['level']);
-            this.component('removeSession', ['level']);
-            lv = lv || this.level;
-
-            this.navContainer.empty();
-
-            var spl, $nav = null, $title = null;
-            for(var i=0; i < lv.length; i++) {
-                spl = lv[i].split(',');
-                $nav = this.navEl.clone();
-                if(i == 0) {
-                    $nav.children('i').addClass('fa-home');
-                } else {
-                    $nav.children('i').addClass('fa-angle-right');
-                }
-
-                if(i == lv.length - 1) {
-                    $title = this.component('element', ['span']).clone();
-                } else {
-                    $title = this.component('element', ['a']).clone();
-                    $title.attr('href', 'javascript:void(0);');
-                }
-
-                $title.attr('data-parentid', spl[0]).text(spl[1]);
-
-                $nav.append($title);
-
-                this.navContainer.append($nav);
-            }
-            this.level = lv;
-        },
-
-        navClick : function(event) {
-            var parentId = this.$(event.target).data('parentid');
-
-            for(var i=this.level.length - 1; i >= 0; i--) {
-                var spl = this.level[i].split(',');
-                if(spl[0] == parentId) {
-                    this.level.splice(i + 1, this.level.length - i - 1);
-
-                    this.component('saveSession', ['level', this.level]);
-                    indexCtrl.loadPage('column/index.html');
-                    break;
-                }
-            }
-        },*/
-
         addBefore : function() {
-            this.component('saveSession', ['level', this.level]);
+            new ResourceAlbumEntity({id : 0, resourceType : this.resourceType}).saveSession('rowInfo');
         },
 
         entryBefore : function(event) {
-            var id = this.getRowId(event);
-            var title = this.$(event.target).parents('tr').find('*[data-field="title"]').text();
-
-            this.level.push(id + ',' + title);
-
-            this.component('saveSession', ['level', this.level]);
-        },
-
-        editBefore : function() {
-            this.component('saveSession', ['level', this.level]);
+            this.operateConfig.entry.page = 'resource/' + this.resourceType + 'List.html';
         },
 
         deleteBefore : function(event) {
             this.config.deleteRow.params.resourceAlbumId = this.getRowId(event);
-        }/*,
-
-        navBlk : function() {
-            return '<li><i class="fa"></i></li>';
-        }*/
+        }
 
     });
 
