2a19030bced44495c98a42b7eed40b8f6cc87870
huanStephen <huanStephen@163.com>
2017-05-25 11:25:05 +0800

    资源集编辑开发完毕

--- manager/resource/resourceAlbumEdit.js
+++ manager/resource/resourceAlbumEdit.js
@@ -15,22 +15,12 @@
 
         elements : {
             //标题
-            /*'h3.page-title > span:first' : 't',
-            //导航
-            'ul.breadcrumb' : 'navContainer',*/
+            'h3.page-title > span:first' : 't',
             //标题
             'input[data-field="name"]' : 'name',
             //描述
             'textarea[data-field="description"]' : 'description'
         },
-
-        /*events : {
-            'click a[data-parentId]' : 'navClick'
-        },*/
-
-        /*blocks : {
-            'navBlk' : 'navEl'
-        },*/
 
         config : {
             getInfo : {
@@ -48,97 +38,52 @@
         },
 
         submitConfig : {
-            addConfigPath : 'column/addColumn',
-            updateConfigPath : 'column/updateColumn'
+            addConfigPath : 'resource/addResourceAlbum',
+            updateConfigPath : 'resource/updateResourceAlbum'
         },
-
-        level : [],
 
         load : function() {
             this.info = new ResourceAlbumEntity();
             this.info.loadSession('rowInfo');
             this.info.removeSession('rowInfo');
 
-            //this.renderNav();
+            if(this.info.id) {
+                if(this.info.resourceType == 'image') {
+                    this.t.text('编辑 图片');
+                }
+                if(this.info.resourceType == 'sound') {
+                    this.t.text('编辑 音频');
+                }
+                if(this.info.resourceType == 'video') {
+                    this.t.text('编辑 视频');
+                }
+                if(this.info.resourceType == 'attachment') {
+                    this.t.text('编辑 附件');
+                }
+            } else {
+                if(this.info.resourceType == 'image') {
+                    this.t.text('添加 图片');
+                }
+                if(this.info.resourceType == 'sound') {
+                    this.t.text('添加 音频');
+                }
+                if(this.info.resourceType == 'video') {
+                    this.t.text('添加 视频');
+                }
+                if(this.info.resourceType == 'attachment') {
+                    this.t.text('添加 附件');
+                }
+            }
 
             if(this.info.id) {
                 this.config.getInfo.params.albumId = this.info.id;
                 this.loadInfo();
             }
-
-        },
-
-        /*renderNav : function() {
-            var lv = this.component('loadSession', ['level']);
-            this.component('removeSession', ['level']);
-            lv = lv || this.level;
-
-            this.navContainer.empty();
-
-            var spl, $nav = null, $title = null;
-            for(var i=0; i < lv.length; i++) {
-                spl = lv[i].split(',');
-                $nav = this.navEl.clone();
-                if(i == 0) {
-                    $nav.children('i').addClass('fa-home');
-                } else {
-                    $nav.children('i').addClass('fa-angle-right');
-                }
-
-                $title = this.component('element', ['a']).clone();
-                $title.attr('href', 'javascript:void(0);');
-
-                $title.attr('data-parentid', spl[0]).text(spl[1]);
-
-                $nav.append($title);
-
-                this.navContainer.append($nav);
-            }
-
-            $nav = this.navEl.clone();
-            $nav.children('i').addClass('fa-angle-right');
-            $title = this.component('element', ['span']).clone();
-            if(this.info.id) {
-                this.t.text('编辑');
-                $title.text('编辑');
-            } else {
-                this.t.text('添加');
-                $title.text('添加');
-            }
-            $nav.append($title);
-
-            this.navContainer.append($nav);
-
-            this.level = lv;
-        },
-
-        navClick : function(event) {
-            var parentId = this.$(event.target).data('parentid');
-
-            for(var i=this.level.length - 1; i >= 0; i--) {
-                var spl = this.level[i].split(',');
-                if(spl[0] == parentId) {
-                    this.level.splice(i + 1, this.level.length - i - 1);
-
-                    this.component('saveSession', ['level', this.level]);
-                    indexCtrl.loadPage('column/index.html');
-                    break;
-                }
-            }
-        },*/
-
-        submitBefore : function() {
-            //this.component('saveSession', ['level', this.level]);
         },
 
         backClick : function() {
-            //this.component('saveSession', ['level', this.level]);
             indexCtrl.loadPage('resource/index.html');
-        }/*,
-
-        navBlk : function() {
-            return '<li><i class="fa"></i></li>';
-        }*/
+        }
 
     });
 
